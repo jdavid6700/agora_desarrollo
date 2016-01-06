4de62366ffe970865838961ed361d9c1c3c390a1
Benjamin Motta <benmotta@gmail.com>
2016-01-05 22:58:40 -0500

    Añadir item consultar contrato en el menu

--- blocks/gui/home/formulario/home.php
+++ blocks/gui/home/formulario/home.php
@@ -52,8 +52,6 @@ $enlace= $this->miConfigurador->getVariableConfiguracion ( 'enlace' );
 
 		echo "<li><a href='" . $redireccion . "'>Objeto a contratar</a></li>";
 
-		
-		
 		//CONTRATO
 		$desenlace = 'pagina=contrato';
 		$enlace = $miConfigurador->configuracion ['enlace'];
@@ -64,8 +62,16 @@ $enlace= $this->miConfigurador->getVariableConfiguracion ( 'enlace' );
 		
 		echo "<li><a href='" . $redireccion . "'>Contrato</a></li>";
 
-
-
+		//CONSULTAR CONTRATO
+		$variable = 'pagina=contrato';
+		$variable.="&opcion=consultar";
+		$enlace = $miConfigurador->configuracion ['enlace'];
+		$variable = $miConfigurador->fabricaConexiones->crypto->codificar($variable);
+		
+        $_REQUEST [$enlace] = $enlace . '=' . $variable;
+        $redireccion = $url . $_REQUEST [$enlace];		
+		
+		echo "<li><a href='" . $redireccion . "'>Consultar Contrato</a></li>";
 
 		
 		$desenlace = 'pagina=desenlace';
