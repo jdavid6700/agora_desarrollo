bf89ac99fccdf5ad57a1a8fdd2bea26340083d51
jdavid6700 <jdavid.6700@gmail.com>
2016-11-29 17:07:59 -0500

    Modificar Modulo de Inhabilidad para Despliegue

--- blocks/proveedor/consultaProveedor/formulario/inhabilidad.php
+++ blocks/proveedor/consultaProveedor/formulario/inhabilidad.php
@@ -26,8 +26,35 @@ class Formulario {
 		 * que lo complementan.
 		 */
 		// Rescatar los datos de este bloque
-		$conexion = "estructura";
-		$esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB ( $conexion );
+		
+		
+		
+		//*************************************************************************** DBMS *******************************
+		//****************************************************************************************************************
+		
+		$conexion = 'estructura';
+		$esteRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
+		
+		$conexion = 'sicapital';
+		$siCapitalRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
+		
+		$conexion = 'centralUD';
+		$centralUDRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
+		
+		$conexion = 'argo_contratos';
+		$argoRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
+		
+		$conexion = 'core_central';
+		$coreRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
+		
+		$conexion = 'framework';
+		$frameworkRecursoDB = $this->miConfigurador->fabricaConexiones->getRecursoDB($conexion);
+		
+		//*************************************************************************** DBMS *******************************
+		//****************************************************************************************************************
+		
+		
+		
 		
 		$esteBloque = $this->miConfigurador->getVariableConfiguracion ( "esteBloque" );
 		
@@ -82,9 +109,9 @@ class Formulario {
 		echo $this->miFormulario->marcoAgrupacion ( 'inicio', $atributos );                
                 
 			//INICIO INFORMACION OBJETO A CONTRATAR
-				echo "<span class='textoElegante textoEnorme textoAzul'>Empresa Proveedor : </span>"; 
+				echo "<span class='textoElegante textoEnorme textoAzul'>Proveedor : </span>"; 
 				echo "<span class='textoElegante textoMediano textoGris'>". $proveedorEspecifico[0]["nom_proveedor"] . "</span></br>"; 
-				echo "<span class='textoElegante textoEnorme textoAzul'>NIT : </span>"; 
+				echo "<span class='textoElegante textoEnorme textoAzul'>Documento : </span>"; 
 				echo "<span class='textoElegante textoMediano textoGris'>". $proveedorEspecifico[0]["num_documento"] . "</span></br>"; 
 				echo "<span class='textoElegante textoEnorme textoAzul'>Correo : </span>"; 
 				echo "<span class='textoElegante textoMediano textoGris'>". $proveedorEspecifico[0]["correo"] . "</span></br>";                 
--- blocks/proveedor/consultaProveedor/script/ready.php
+++ blocks/proveedor/consultaProveedor/script/ready.php
@@ -34,7 +34,7 @@ $(function() {
 
 	
 $('#<?php echo $this->campoSeguro('fecha')?>').datepicker({
-        dateFormat: 'yy-mm-dd',
+        dateFormat: 'dd/mm/yy',
         changeYear: true,
         monthNames: ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre'],
 		monthNamesShort: ['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Sep','Oct','Nov','Dic'],
